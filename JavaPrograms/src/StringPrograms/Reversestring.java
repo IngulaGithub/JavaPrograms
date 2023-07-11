94cf9c0b402d14df146acd0db15c66283f1c9296
IngulaGithub <137319290+IngulaGithub@users.noreply.github.com>
2023-07-11 20:27:59 +0530

    Update Reversestring.java

--- JavaPrograms/src/StringPrograms/Reversestring.java
+++ JavaPrograms/src/StringPrograms/Reversestring.java
@@ -5,8 +5,7 @@ public class Reversestring {
 	public static void main(String[] args) {
 		System.out.println("Reverse string");
 
-System.out.println("bull");
-System.out.println("pulllllllllllll");
+//pulllllll
 	}
 	}
 
